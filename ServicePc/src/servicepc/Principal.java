fdb8fb3176f33b52e371dbadbb944bd124437077
chelo154 <chelo154@gmail.com>
2019-12-03 00:16:26 -0300

    Subi el Programa de ServicePC al Repositorio

---
+++ ServicePc/src/servicepc/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicepc;
+
+/**
+ *
+ * @author Chelo
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    /*----------Clase Principal donde Se Ejecuta el Programa-------------*/
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaService ventana = new VentanaService();
+        //ahora va sa ver para que hice eso e
+    }
+    
+}
